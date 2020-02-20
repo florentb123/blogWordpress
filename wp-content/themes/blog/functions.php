1c0ee40ccc1abbffa06edadd825195f0452e2348
ericc70 <eric.c@codeur.online>
2020-02-20 13:59:15 +0000

    struture template v1

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -1,1 +1,36 @@
-qfdvsfdv
+<?php
+function add_theme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+   
+   // wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all');
+   
+   // wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+   
+     //
+  }
+  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+
+  // Register a new sidebar simply named 'sidebar'
+function add_widget_Support() {
+    register_sidebar( array(
+                    'name'          => 'Sidebar',
+                    'id'            => 'sidebar',
+                    'before_widget' => '<div>',
+                    'after_widget'  => '</div>',
+                    'before_title'  => '<h2>',
+                    'after_title'   => '</h2>',
+    ) );
+}
+// Hook the widget initiation and run our function
+add_action( 'widgets_init', 'add_Widget_Support' );
+
+
+// Register a new navigation menu
+function add_Main_Nav() {
+    register_nav_menu('header-menu',__( 'Header Menu' ));
+  }
+  // Hook to the init action hook, run our navigation menu function
+  add_action( 'init', 'add_Main_Nav' );
+
+  
