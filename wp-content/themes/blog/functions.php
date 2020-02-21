9ca74b9e3c7e011eb56f8eef1dfab9f018fb8e0a
ericc70 <eric.c@codeur.online>
2020-02-21 14:16:38 +0000

    add function

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -35,6 +35,8 @@ function add_Main_Nav() {
 
 
   add_theme_support( 'post-thumbnails' );
+
+
 function wpm_custom_post_type() {
     
 	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
@@ -151,3 +153,254 @@ function apprenants_save_meta_box_data( $post_id ){
 	
 }
 add_action( 'save_post_apprenants', 'apprenants_save_meta_box_data' );
+
+/**
+ * projets
+ */
+
+function projet_custom_post_type() {
+    
+	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
+	$labels = array(
+		// Le nom au pluriel
+		'name'                => _x( 'Projets ', 'Post Type General Name'),
+		// Le nom au singulier
+		'singular_name'       => _x( 'Projet ', 'Post Type Singular Name'),
+		// Le libellé affiché dans le menu
+		'menu_name'           => __( 'Projets '),
+		// Les différents libellés de l'administration
+		'all_items'           => __( 'Tous les Projets'),
+		'view_item'           => __( 'Voir les Projets'),
+		'add_new_item'        => __( 'Ajouter un nouvel proj'),
+		'add_new'             => __( 'Ajouter'),
+		'edit_item'           => __( 'Editer'),
+		'update_item'         => __( 'Modifier'),
+		'search_items'        => __( 'Rechercher'),
+		'not_found'           => __( 'Non trouvée'),
+		'not_found_in_trash'  => __( 'Non trouvée dans la corbeille'),
+	);
+	
+	// On peut définir ici d'autres options pour notre custom post type
+	
+	$args = array(
+		'label'               => __( 'Projets'),
+		'description'         => __( 'Tous sur Projets'),
+		'labels'              => $labels,
+		// On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
+		'supports'            => array( 'title', 'editor', 'excerpt',  'thumbnail',  'revisions', 'custom-fields', ),
+		/* 
+		* Différentes options supplémentaires
+		*/	
+		
+		'show_in_rest' => true,
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			  => array( 'slug' => 'Projets'),
+
+	);
+	
+	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
+	register_post_type( 'Projets', $args );
+
+}
+
+add_action( 'init', 'projet_custom_post_type', 0 );
+
+
+function projets_add_meta_boxes( $post ){
+    add_meta_box( 'projets_meta_box', __( 'projet', 'projets_example_plugin' ), 'projets_build_meta_box', 'projets', 'normal', 'low' );
+}
+add_action( 'add_meta_boxes_projets', 'projets_add_meta_boxes' );
+
+function projets_build_meta_box( $post ){
+	// make sure the form request comes from WordPress
+	wp_nonce_field( basename( __FILE__ ), 'projets_meta_box_nonce' );
+
+	// retrieve the _projets_git current value
+	$url_projet = get_post_meta( $post->ID, '_url_projet', true );
+
+	// retrieve the _projets_linkedin current value
+	$desciption_projet = get_post_meta( $post->ID, '_projets_description', true );
+
+	
+	?>
+	<div class='inside'>
+
+		<h3><?php _e( 'Url du projet', 'projets_example_plugin' ); ?></h3>
+		<p>
+			<input type="text" name="url" value="<?php echo $url_projet; ?>" /> 
+		</p>
+
+		<h3><?php _e( 'Description du projet', 'projets_example_plugin' ); ?></h3>
+		<p>
+			<input type="text" name="description"  value="<?php echo $desciption_projet; ?>" /> 
+		</p>
+
+		
+	</div>
+	<?php
+}
+
+
+
+function projets_save_meta_box_data( $post_id ){
+	// verify taxonomies meta box nonce
+	if ( !isset( $_POST['projets_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['projets_meta_box_nonce'], basename( __FILE__ ) ) ){
+		return;
+	}
+
+	// return if autosave
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ){
+		return;
+	}
+
+	// store custom fields values
+	// git string
+	if ( isset( $_REQUEST['url'] ) ) {
+		update_post_meta( $post_id, '_url_projet', sanitize_text_field( $_POST['url'] ) );
+	}
+	
+	// store custom fields values
+	// linkedin string
+	if ( isset( $_REQUEST['description'] ) ) {
+		update_post_meta( $post_id, '_projets_description', sanitize_text_field( $_POST['description'] ) );
+	}
+	
+	
+}
+add_action( 'save_post_projets', 'projets_save_meta_box_data' );
+
+
+/**
+ * Filter the except length to 20 characters.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+
+
+/**
+ * Galleries
+ */
+
+function gallery_custom_post_type() {
+    
+	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
+	$labels = array(
+		// Le nom au pluriel
+		'name'                => _x( 'Galerries ', 'Post Type General Name'),
+		// Le nom au singulier
+		'singular_name'       => _x( 'Galerries ', 'Post Type Singular Name'),
+		// Le libellé affiché dans le menu
+		'menu_name'           => __( 'Galerries '),
+		// Les différents libellés de l'administration
+		'all_items'           => __( 'Tous les Galerries'),
+		'view_item'           => __( 'Voir les Galerries'),
+		'add_new_item'        => __( 'Ajouter un nouvel proj'),
+		'add_new'             => __( 'Ajouter'),
+		'edit_item'           => __( 'Editer'),
+		'update_item'         => __( 'Modifier'),
+		'search_items'        => __( 'Rechercher'),
+		'not_found'           => __( 'Non trouvée'),
+		'not_found_in_trash'  => __( 'Non trouvée dans la corbeille'),
+	);
+	
+	// On peut définir ici d'autres options pour notre custom post type
+	
+	$args = array(
+		'label'               => __( 'Galerries'),
+		'description'         => __( 'Tous sur Galerries'),
+		'labels'              => $labels,
+		// On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
+		'supports'            => array( 'title', 'editor', 'excerpt',  'thumbnail',  'revisions', 'custom-fields', ),
+		/* 
+		* Différentes options supplémentaires
+		*/	
+		
+		'show_in_rest' => true,
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			  => array( 'slug' => 'Gallerys'),
+
+	);
+	
+	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
+	register_post_type( 'Gallerys', $args );
+
+}
+
+add_action( 'init', 'gallery_custom_post_type', 0 );
+
+
+function gallerys_add_meta_boxes( $post ){
+    add_meta_box( 'gallerys_meta_box', __( 'gallery', 'gallerys_example_plugin' ), 'gallerys_build_meta_box', 'gallerys', 'normal', 'low' );
+}
+add_action( 'add_meta_boxes_gallerys', 'gallerys_add_meta_boxes' );
+
+function gallerys_build_meta_box( $post ){
+	// make sure the form request comes from WordPress
+	wp_nonce_field( basename( __FILE__ ), 'gallerys_meta_box_nonce' );
+
+
+
+	// retrieve the _gallerys_linkedin current value
+	$desciption_gallery = get_post_meta( $post->ID, '_gallerys_description', true );
+
+	
+	?>
+	<div class='inside'>
+
+	
+
+		<h3><?php _e( 'Description de la photo', 'gallerys_example_plugin' ); ?></h3>
+		<p>
+			<input type="text" name="description"  value="<?php echo $desciption_gallery; ?>" /> 
+		</p>
+
+		
+	</div>
+	<?php
+}
+
+
+
+function gallerys_save_meta_box_data( $post_id ){
+	// verify taxonomies meta box nonce
+	if ( !isset( $_POST['gallerys_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['gallerys_meta_box_nonce'], basename( __FILE__ ) ) ){
+		return;
+	}
+
+	// return if autosave
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ){
+		return;
+	}
+
+	// store custom fields values
+
+	
+	// store custom fields values
+	// linkedin string
+	if ( isset( $_REQUEST['description'] ) ) {
+		update_post_meta( $post_id, '_gallerys_description', sanitize_text_field( $_POST['description'] ) );
+	}
+	
+	
+}
+add_action( 'save_post_gallerys', 'gallerys_save_meta_box_data' );
--- wp-content/themes/blog/index.php
+++ wp-content/themes/blog/index.php
@@ -1,19 +1,88 @@
 <?php get_header(); ?>
 <main class="wrap">
-  <section class="content-area content-thin">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <article class="article-loop">
-        <header>
-          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-          By: <?php the_author(); ?>
-        </header>
-        <?php the_excerpt(); ?>
-      </article>
-<?php endwhile; else : ?>
-      <article>
-        <p>Sorry, no posts were found!</p>
-      </article>
-<?php endif; ?>
-  </section><?php get_sidebar(); ?>
+  <div class="content-area content-thin">
+
+ <section> <h2>Derniers articles</h2>
+
+<?php
+    $recentPosts = new WP_Query();
+    $recentPosts->query('showposts=3');
+?>
+<?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+
+   <!-- Ce qui suit teste si l'Article en cours est dans la Catégorie 3. -->
+ <!-- Si c'est le cas, le bloc div reçoit la classe CSS "post-cat-three". -->
+ <!-- Sinon, le bloc div reçoit la classe CSS "post". -->
+
+ <div class="entry">
+ <!-- Affiche le Titre en tant que lien vers le Permalien de l'Article. -->
+ <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+
+
+ <!-- Affiche le corps (Content) de l'Article dans un bloc div. -->
+
+
+   <?php
+   the_post_thumbnail(); 
+   /*
+   Affiche les 20 premiers caracters de l'article (nbre de caractere defini dans functions.php)
+   **/
+   the_excerpt(); 
+
+   /**
+   * affiche l'article en entier
+   */
+  //the_content(); ?>
+ </div>
+
+ <!-- Affiche une liste des Catégories des Articles séparées par des virgules. -->
+ <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
+ </div> <!-- Fin du premier bloc div -->
+ 
+ <!-- Fin de La Boucle (mais notez le "else:" - voir la suite). -->
+ 
+
+ 
+
+<?php endwhile; ?>
+</section>
+<section>
+  <h3>Projets</h3>
+
+  <?php
+ $args = array(
+    'post_type' =>'projets',
+    
+ );
+
+            $featured_query = new WP_Query($args);
+           
+           // limit 3 derniers projets
+        $featured_query->set('posts_per_page', 3);
+        $featured_query->query($featured_query->query_vars);
+// var_dump($featured_query);
+                     while ($featured_query->have_posts()) : $featured_query->the_post();
+                
+                   $url =  get_post_meta( get_the_ID(), '_url_projet' , true );
+                   $projet =  get_post_meta( get_the_ID(), '_projets_description' , true );
+
+             
+          the_title();
+          echo '<br>';
+          the_post_thumbnail(); 
+          echo '<br>';
+           echo $projet;
+           echo '<br>';
+           echo $url;
+           echo '<br>'; echo '<br>';
+endwhile;
+
+    ?>
+</section>
+
+</div><?php get_sidebar(); ?>
+
 </main>
 <?php get_footer(); ?>
+
