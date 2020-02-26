82b0f6622fa5da95a4d5813cb08bd617d66f444c
florentb123 <bonneau.florent44gmail.com>
2020-02-26 15:37:53 +0000

    jspas

--- wp-content/themes/blog/front-page.php
+++ wp-content/themes/blog/front-page.php
@@ -2,7 +2,7 @@
   <main class="wrap">
     <section class="content-area content-thin">
       <div class="container-fluid divGallery">
-        <div class="row">
+        <div class="row overflow-hidden">
           <?php if (is_active_sidebar('gallery1')) : ?>
             <div id="gallery1" class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0" >
             <?php dynamic_sidebar('gallery1'); 
@@ -37,5 +37,22 @@
       </div>
       <?php get_template_part('content','') ?>
     </section>
+    <section>
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 px-0">
+              <div id="map">
+              </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-8 col-lg-6 col-xl-6 px-0">
+              <div class="bg-emplacement"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
+  <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/map.js"></script>
 <?php get_footer(); ?>
--- wp-content/themes/blog/projets.php
+++ wp-content/themes/blog/projets.php
@@ -7,43 +7,52 @@
 
 <main class="wrap">
         
+<div class="container-fluid bg-grey">
+          <div class="container">
+            <div class="row pt-5  ">
+              <div class="col-12 p-0 ">
+                <h2 class="text-center title-playfaire">Nos projets</h2>
+              </div>
+            </div>
+            <div class="row p-0 pt-5 justify-content-between">
+<?php
+              $args = array(
+                'post_type' => 'projets',
+              );
 
-  <?php while ( have_posts() ) : the_post();
-    if( has_post_thumbnail() ):
-        echo get_the_post_thumbnail();
-    endif;  
-endwhile; ;
-  $args = array(
-    'post_type' => 'apprenants',
-    'posts_per_page' => 20,
-    'order' => 'ASC',
-  );
+              $featured_query = new WP_Query($args);
 
-  $my_query = new WP_Query($args);
-  ?>
-    <div class="container-fluid">
-        <div class="container">
-            <div class="row">
-            
-        <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-            <?php
-            $git = get_post_meta(get_the_ID(), '_apprenants_git', true);
-            $linkedin = get_post_meta(get_the_ID(), '_apprenants_linkedin', true);
-            ?>
-            <div class="col-3">
-                <div class="bg-apprenant"><?php the_post_thumbnail('post-thumbnail',''); ?></div>
-                <?php the_title("<h5>", "</h5>"); ?>
-                <div class="d-flex">
-                <a href="<?php echo $git ; ?>"><div class="iconGit"></div></a>
-                <a href="<?php echo $linkedin ; ?>"><div class="iconLinkedin"></div></a>
-                </div>
+              // limit 3 derniers projets
+              $featured_query->query($featured_query->query_vars);
+              // var_dump($featured_query);
+
+              if ($featured_query->have_posts()) :while ($featured_query->have_posts()) : $featured_query->the_post(); 
+              $url =  get_post_meta(get_the_ID(), '_url_projet', true);
+              $projet =  get_post_meta(get_the_ID(), '_projets_description', true); ?>
                 
+                  <div class="bg-white articles-mw p-0 Regular shadow mb-5">
+                    <header>
+                      <a href="<?php the_permalink(); ?>"><div class="bg-articles " style="background-image:url(<?php echo get_the_post_thumbnail_url() ?>)"></div></a>
+                      <h2 class="w-75 m-auto py-3 text-center title-playfaire ">
+                        <a  href="<?php the_permalink(); ?>" class="text-body" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                      </h2>
+                    
+                    <?php echo '<p class="w-75 m-auto pb-5 textProjet text-playfaire ">'.get_the_excerpt().'</p>' ?>
+                    </header>
+                  </div>
+                
+              <?php endwhile; ?>
+
             </div>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-            </div>
+          </div>
         </div>
-    </div>
+      </article>
+      <?php else : ?>
+      
+        <article>
+          <p >Sorry, no posts were found!</p>
+        </article>
+      <?php endif; ?>
 
 </main>
 <?php get_footer(); ?>
