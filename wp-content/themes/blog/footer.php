1e92dbdf3cd052d7d0694b27caeea4afaa49e85e
anthony39100 <anthonybreviglieri.ab@gmail.com>
2020-02-27 15:24:41 +0000

    ajout de la page articles

--- wp-content/themes/blog/footer.php
+++ wp-content/themes/blog/footer.php
@@ -1,5 +1,10 @@
 <footer>
-      <p>Copyright &copy; 2017</p>
+<div class="row vh-20">
+        <div class="col-12 d-flex flex-column justify-content-center align-items-center">
+          <p class="mt-5"><img src="assets/images/LOGOGIGITBREAKERS.png"
+              class="img-fluid" alt="Logo de la promotion gitbreaker" width="" height=""></p>
+              <p>Anthony Breviglieri, Aurélie Nourdine, Eric Chaon, Florent Bonneau</p>
+        </div>
     </footer>
     <?php wp_footer(); ?>
   </body>
