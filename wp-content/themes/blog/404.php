4d5a003b61d50a08ea8a5efd5621058fc9a75ec0
florentb123 <bonneau.florent44gmail.com>
2020-02-28 13:53:43 +0000

    manque 404 et liens automotive

--- wp-content/themes/blog/404.php
+++ wp-content/themes/blog/404.php
@@ -1,1 +1,6 @@
-404 not found
+<?php get_header(); ?>
+
+
+<div style="background-image: url('https://ericc.promo-vesoul33.codeur.online/gitbreakers/wp-content/uploads/2020/02/221068-P1O4NN-458.jpg'); height:200vh; background-size:cover; background-position:center;">
+
+</div> 
